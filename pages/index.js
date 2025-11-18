c9336dc3580b96a8708418f263e674ef093dc434
CELSO LUIZ DA SILVA JUNIOR <63802267+celsojuniorsfs@users.noreply.github.com>
2025-11-18 00:38:58 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Andreza te amo ❤, se você me ama me da um beijinho</h1>;
+  return <h1>Andreza te amo ❤, se você me ama me da um beijinho</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
