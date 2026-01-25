5c5f9daf033f98ccaa229817962c504c356d14bc
Celso Junior <celsojunior.vmagro@gmail.com>
2026-01-24 21:24:13 -0300

    adiciona testes para a função de soma da calculadora

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora");
+const calculadora = require("../../models/calculadora");
 
 test("somar 2 + 2 deveria retornar 4", () => {
   expect(calculadora.somar(2, 2)).toBe(4);
